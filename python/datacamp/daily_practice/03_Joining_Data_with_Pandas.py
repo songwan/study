d380a2eec1b7697c667bbbbae3e8f3e5a4d82fb2
Songwan Joun <songwan.joun@gmail.com>
2021-06-29 22:17:24 +0900

    Finish python JOINs

--- python/datacamp/daily_practice/03_Joining_Data_with_Pandas.py
+++ python/datacamp/daily_practice/03_Joining_Data_with_Pandas.py
@@ -82,3 +82,131 @@ print(movies_ratings.head())
 
 # Merge sequels and financials on index id
 sequels_fin = sequels.merge(financials, on='id', how='outer')
+
+# Merge employees and top_cust
+empl_cust = employees.merge(top_cust, on='srid', 
+                            how='left', indicator=True)
+
+# Merge the non_mus_tck and top_invoices tables on tid
+tracks_invoices = non_mus_tcks.merge(top_invoices, on='tid')
+
+# Use .isin() to subset non_mus_tcks to rows with tid in tracks_invoices
+top_tracks = non_mus_tcks[non_mus_tcks['tid'].isin(tracks_invoices.tid)]
+
+# Group the top_tracks by gid and count the tid rows
+cnt_by_gid = top_tracks.groupby(['gid'], as_index=False).agg({'tid':'count'})
+
+# Merge the genres table to cnt_by_gid on gid and print
+print(cnt_by_gid.merge(genres, on='gid'))
+
+# Concatenate the tracks
+tracks_from_albums=pd.concat([tracks_master,tracks_ride,tracks_st],sort=True)
+print(tracks_from_albums)
+
+# Concatenate the tables and add keys
+inv_jul_thr_sep = pd.concat([inv_jul,inv_aug,inv_sep], 
+                            keys=('7Jul','8Aug','9Sep'))
+
+# Group the invoices by the index keys and find avg of the total column
+avg_inv_by_month = inv_jul_thr_sep.groupby(level=0).agg({'total':'mean'})
+
+# Bar plot of avg_inv_by_month
+avg_inv_by_month.plot(kind='bar')
+plt.show()
+
+# Use the .append() method to combine the tracks tables
+metallica_tracks = tracks_ride.append([tracks_master,tracks_st], sort=False)
+
+# Merge metallica_tracks and invoice_items
+tracks_invoices = metallica_tracks.merge(invoice_items, on='tid', how='inner')
+
+# For each tid and name sum the quantity sold
+tracks_sold = tracks_invoices.groupby(['tid','name']).agg({'quantity':'sum'})
+
+# Sort in decending order by quantity and print the results
+print(tracks_sold.sort_values('quantity', ascending=False))
+
+# Concatenate the classic tables vertically
+classic_18_19 = pd.concat([classic_18,classic_19], axis=0, ignore_index=True)
+
+# Concatenate the pop tables vertically
+pop_18_19 = pd.concat([pop_18,pop_19], axis=0, ignore_index=True)
+
+# Use merge_ordered() to merge gdp and sp500 on year and date
+gdp_sp500 = pd.merge_ordered(gdp, sp500, left_on='year', right_on='date', 
+                             how='left')
+
+# Print gdp_sp500
+print(gdp_sp500)
+
+# Use merge_ordered() to merge inflation, unemployment with inner join
+inflation_unemploy = pd.merge_ordered(inflation, unemployment, how='inner', on='date')
+
+# Print inflation_unemploy 
+print(inflation_unemploy)
+
+# Plot a scatter plot of unemployment_rate vs cpi of inflation_unemploy
+inflation_unemploy.plot(x='unemployment_rate',y='cpi',kind='scatter')
+plt.show()
+
+# Merge gdp and pop on date and country with fill and notice rows 2 and 3
+ctry_date = pd.merge_ordered(gdp, pop, on=('date','country'), 
+                             fill_method='ffill')
+
+# Print ctry_date
+print(ctry_date)
+
+# Use merge_asof() to merge jpm and wells
+jpm_wells = pd.merge_asof(jpm, wells, on='date_time', suffixes=('','_wells'), direction='nearest')
+
+
+# Use merge_asof() to merge jpm_wells and bac
+jpm_wells_bac = pd.merge_asof(jpm_wells, bac, on='date_time', suffixes=('_jpm','_bac'), direction='nearest')
+
+
+# Compute price diff
+price_diffs = jpm_wells_bac.diff()
+
+# Plot the price diff of the close of jpm, wells and bac only
+price_diffs.plot(y=['close_jpm','close_wells','close_bac'])
+plt.show()
+
+# Merge gdp and recession on date using merge_asof()
+gdp_recession = pd.merge_asof(gdp, recession, on='date')
+
+# Create a list based on the row value of gdp_recession['econ_status']
+is_recession = ['r' if s=='recession' else 'g' for s in gdp_recession['econ_status']]
+
+# Plot a bar chart of gdp_recession
+gdp_recession.plot(kind='bar', y='gdp', x='date', color=is_recession, rot=90)
+plt.show()
+
+# Merge gdp and pop on date and country with fill
+gdp_pop = pd.merge_ordered(gdp, pop, on=('country', 'date'), fill_method='ffill')
+
+# unpivot everything besides the year column
+ur_tall = ur_wide.melt(id_vars='year', var_name = 'month', value_name = 'unempl_rate')
+
+# Create a date column using the month and year columns of ur_tall
+ur_tall['date'] = pd.to_datetime(ur_tall['year'] + '-' + ur_tall['month'])
+
+# Sort ur_tall by date in ascending order
+ur_sorted = ur_tall.sort_values('date', ascending=True)
+
+# Plot the unempl_rate by date
+ur_sorted.plot(x='date',y='unempl_rate')
+plt.show()
+
+# Use melt on ten_yr, unpivot everything besides the metric column
+bond_perc = ten_yr.melt(id_vars='metric', var_name='date', value_name='close')
+
+# Use query on bond_perc to select only the rows where metric=close
+bond_perc_close = bond_perc.query("metric=='close'")
+
+# Merge (ordered) dji and bond_perc_close on date with an inner join
+dow_bond = pd.merge_ordered(dji, bond_perc_close, on='date', how='inner',suffixes=('_dow','_bond'))
+
+
+# Plot only the close_dow and close_bond columns
+dow_bond.plot(y=['close_dow','close_bond'], x='date', rot=90)
+plt.show()

c24234816ae757f3129947e9584d9d69874b0aa3
Songwan Joun <songwan.joun@gmail.com>
2021-07-02 19:11:56 +0900

    Practice DB in Python

---
+++ python/datacamp/daily_practice/B5_Introduction_to_Databases_in_Python.py
@@ -0,0 +1,77 @@
+# Import create_engine
+from sqlalchemy import create_engine
+
+# Create an engine that connects to the census.sqlite file: engine
+engine = create_engine('sqlite:///census.sqlite')
+
+# Print table names
+print(engine.table_names())
+
+
+# Import create_engine, MetaData, and Table
+from sqlalchemy import create_engine, MetaData, Table
+
+# Create engine: engine
+engine = create_engine('sqlite:///census.sqlite')
+
+# Create a metadata object: metadata
+metadata = MetaData()
+
+# Reflect census table from the engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Print census table metadata
+print(repr(census))
+
+from sqlalchemy import create_engine, MetaData, Table
+
+engine = create_engine('sqlite:///census.sqlite')
+
+metadata = MetaData()
+
+# Reflect the census table from the engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Print the column names
+print(census.columns.keys())
+
+# Print full metadata of census
+print(repr(census))
+
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///census.sqlite')
+
+# Create a connection on engine
+connection = engine.connect()
+
+# Build select statement for census table: stmt
+stmt = 'SELECT * FROM census'
+
+# Execute the statement and fetch the results: results
+results = connection.execute(stmt).fetchall()
+
+# Print results
+print(results)
+
+# Import select
+from sqlalchemy import select
+
+# Reflect census table via engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Build select statement for census table: stmt
+stmt = select([census])
+
+# Print the emitted statement to see the SQL string
+print(stmt)
+
+# Execute the statement on connection and fetch 10 records: result
+results = connection.execute(stmt).fetchmany(size=10)
+
+# Execute the statement and print the results
+print(results)
+
+first_row = results[0]
+print(first_row)
+print(first_row[0])
+print(first_row.state)

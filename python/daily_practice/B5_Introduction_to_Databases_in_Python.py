6faaf73994b5d8b81be8b1f0ba2fc768463b5a63
Songwan Joun <songwan.joun@gmail.com>
2021-07-05 17:08:33 +0900

    Practice DB creation and manipulation

--- python/daily_practice/B5_Introduction_to_Databases_in_Python.py
+++ python/daily_practice/B5_Introduction_to_Databases_in_Python.py
@@ -394,3 +394,272 @@ results_proxy.close()
 
 # Print the count by state
 print(state_count)
+
+# Import Table, Column, String, Integer, Float, Boolean from sqlalchemy
+from sqlalchemy import Table, Column, String, Integer, Float, Boolean
+
+# Define a new table with a name, count, amount, and valid column: data
+data = Table('data', metadata,
+             Column('name', String(255)),
+             Column('count', Integer()),
+             Column('amount', Float()),
+             Column('valid', Boolean())
+)
+
+# Use the metadata to create the table
+metadata.create_all(engine)
+
+# Print table details
+print(repr(data))
+
+
+# Import Table, Column, String, Integer, Float, Boolean from sqlalchemy
+from sqlalchemy import Table, Column, String, Integer, Float, Boolean
+
+# Define a new table with a name, count, amount, and valid column: data
+data = Table('data', metadata,
+             Column('name', String(255), unique=True),
+             Column('count', Integer(), default=1),
+             Column('amount', Float()),
+             Column('valid', Boolean(), default=False)
+)
+
+# Use the metadata to create the table
+metadata.create_all(engine)
+
+# Print the table details
+print(repr(metadata.tables['data']))
+
+# Import insert and select from sqlalchemy
+from sqlalchemy import insert, select
+
+# Build an insert statement to insert a record into the data table: insert_stmt
+insert_stmt = insert(data).values(name='Anna', count=1, amount=1000.00, valid=True)
+
+# Execute the insert statement via the connection: results
+results = connection.execute(insert_stmt)
+
+# Print result rowcount
+print(results.rowcount)
+
+# Build a select statement to validate the insert: select_stmt
+select_stmt = select([data]).where(data.columns.name == 'Anna')
+
+# Print the result of executing the query.
+print(connection.execute(select_stmt).first())
+
+# Build a list of dictionaries: values_list
+values_list = [
+    {'name':'Anna','count':1,'amount':1000.00,'valid':True},
+    {'name':'Taylor','count':1,'amount':750.00,'valid':False}]
+
+# Build an insert statement for the data table: stmt
+stmt = insert(data)
+
+# Execute stmt with the values_list: results
+results = connection.execute(stmt, values_list)
+
+# Print rowcount
+print(results.rowcount)
+
+# import pandas
+import pandas as pd
+
+# read census.csv into a DataFrame : census_df
+census_df = pd.read_csv('census.csv', header=None)
+
+# rename the columns of the census DataFrame
+census_df.columns = ['state', 'sex', 'age', 'pop2000', 'pop2008']
+
+# Build a select statement: select_stmt
+select_stmt = select([state_fact]).where(state_fact.columns.name == 'New York')
+
+# Execute select_stmt and fetch the results
+results = connection.execute(select_stmt).fetchall()
+
+# Print the results of executing the select_stmt
+print(results)
+
+# Print the FIPS code for the first row of the result
+print(results[0]['fips_state'])
+# Build a statement to update the notes to 'The Wild West': stmt
+stmt = update(state_fact).values(notes='The Wild West')
+
+# Append a where clause to match the West census region records: stmt_west
+stmt_west = stmt.where(state_fact.columns.census_region_name == 'West')
+
+# Execute the statement: results
+results = connection.execute(stmt_west)
+
+# Print rowcount
+print(results.rowcount)
+
+# Build a statement to select name from state_fact: fips_stmt
+fips_stmt = select([state_fact.columns.name])
+
+# Append a where clause to match the fips_state to flat_census fips_code: fips_stmt
+fips_stmt = fips_stmt.where(
+    state_fact.columns.fips_state == flat_census.columns.fips_code)
+
+# Build an update statement to set the name to fips_stmt_where: update_stmt
+update_stmt = update(flat_census).values(state_name=fips_stmt)
+
+# Execute update_stmt: results
+results = connection.execute(update_stmt)
+
+# Print rowcount
+print(results.rowcount)
+
+
+# Import delete, select
+from sqlalchemy import delete, select
+
+# Build a statement to empty the census table: stmt
+delete_stmt = delete(census)
+
+# Execute the statement: results
+results = connection.execute(delete_stmt)
+
+# Print affected rowcount
+print(results.rowcount)
+
+# Build a statement to select all records from the census table : select_stmt
+select_stmt = select([census])
+
+# Print the results of executing the statement to verify there are no rows
+print(connection.execute(select_stmt).fetchall())
+
+
+# Build a statement to count records using the sex column for Men ('M') age 36: count_stmt
+count_stmt = select([func.count(census.columns.sex)]).where(
+    and_(census.columns.sex == 'M',
+         census.columns.age == 36)
+)
+
+# Execute the select statement and use the scalar() fetch method to save the record count
+to_delete = connection.execute(count_stmt).scalar()
+
+# Build a statement to delete records from the census table: delete_stmt
+delete_stmt = delete(census)
+
+# Append a where clause to target Men ('M') age 36: delete_stmt
+delete_stmt = delete_stmt.where(
+    and_(census.columns.sex == 'M',
+         census.columns.age == 36)
+)
+
+# Execute the statement: results
+results = connection.execute(delete_stmt)
+
+# Print affected rowcount and to_delete record count, make sure they match
+print(results.rowcount, to_delete)
+
+# Drop the state_fact table
+state_fact.drop(engine)
+
+# Check to see if state_fact exists
+print(state_fact.exists(engine))
+
+# Drop all tables
+metadata.drop_all(engine)
+
+# Check to see if census exists
+print(engine.table_names())
+
+# Import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData
+
+# Define an engine to connect to chapter5.sqlite: engine
+engine = create_engine('sqlite:///chapter5.sqlite')
+
+# Initialize MetaData: metadata
+metadata = MetaData()
+
+# Import Table, Column, String, and Integer
+from sqlalchemy import Table, Column, String, Integer
+
+# Build a census table: census
+census = Table('census', metadata,
+               Column('state', String(30)),
+               Column('sex', String(1)),
+               Column('age', Integer()),
+               Column('pop2000', Integer()),
+               Column('pop2008', Integer()))
+
+# Create the table in the database
+metadata.create_all(engine)
+
+# Create an empty list: values_list
+values_list = []
+
+# Iterate over the rows
+for row in csv_reader:
+    # Create a dictionary with the values
+    data = {'state': row[0], 'sex': row[1], 'age':row[2], 'pop2000': row[3],
+            'pop2008': row[4]}
+    # Append the dictionary to the values list
+    values_list.append(data)
+# Import insert
+from sqlalchemy import insert
+
+# Build insert statement: stmt
+stmt = insert(census)
+
+# Use values_list to insert data: results
+results = connection.execute(stmt, values_list)
+
+# Print rowcount
+print(results.rowcount)
+
+
+# Import select and func
+from sqlalchemy import select, func
+
+# Select the average of age weighted by pop2000
+stmt = select([
+	func.sum(census.columns.age * census.columns.pop2000) / func.sum(census.columns.pop2000)
+
+			  ])
+
+# import case, cast and Float from sqlalchemy
+from sqlalchemy import case, cast, Float
+
+# Build a query to calculate the percentage of women in 2000: stmt
+stmt = select([census.columns.state,
+    (func.sum(
+        case([
+            (census.columns.sex == 'F', census.columns.pop2000)
+        ], else_=0)) /
+     cast(func.sum(census.columns.pop2000), Float) * 100).label('percent_female')
+])
+
+# Group By state
+stmt = stmt.group_by(census.columns.state)
+
+# Execute the query and store the results: results
+results = connection.execute(stmt).fetchall()
+
+# Print the percentage
+for result in results:
+    print(result.state, result.percent_female)
+
+# Build query to return state name and population difference from 2008 to 2000
+stmt = select([census.columns.state,
+     (census.columns.pop2008-census.columns.pop2000).label('pop_change')
+])
+
+# Group by State
+stmt = stmt.group_by(census.columns.state)
+
+# Order by Population Change
+stmt = stmt.order_by(desc('pop_change'))
+
+# Limit to top 10
+stmt = stmt.limit(10)
+
+# Use connection to execute the statement and fetch all results
+results = connection.execute(stmt).fetchall()
+
+# Print the state and population change for each record
+for result in results:
+    print('{}:{}'.format(result.state, result.pop_change))
